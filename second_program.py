923d70530d24657d8b10f44b223273438a37a78a
trollybus <eliubaevramazan.99@gmail.com>
2021-03-31 13:32:14 +0700

    second_program_optimization

--- second_program.py
+++ second_program.py
@@ -4,6 +4,7 @@ import first_program
 arrays = ([1, 2, 3, 4, 5], [12, 4, 1, 5, 8, 10], [11, 23, 1, 7, 3, 4, 1, 9, 10])
 
 if __name__ == '__main__':
+    processes = []
     for s_arr in arrays:
         MeanProcess = Process(target=first_program.mean_of_array, args=[s_arr])
         VarProcess = Process(target=first_program.var_of_array, args=[s_arr])
@@ -11,7 +12,9 @@ if __name__ == '__main__':
         MeanProcess.start()
         VarProcess.start()
 
-        MeanProcess.join()
-        VarProcess.join()
+        processes.append(MeanProcess)
+        processes.append(VarProcess)
+
+    [process.join() for process in processes]
 
     print('NICE')
